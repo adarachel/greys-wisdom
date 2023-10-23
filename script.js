58349c01f5c0d5f2155c4de27f3a5deccdbbb990
Ada Rachel <adarachelofficial@gmail.com>
2023-10-23 11:48:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://greyswisdomapi.onrender.com/greys-wisdom-api';
+const API_URL = 'https://greyswisdomapi.onrender.com/get';
 
 // Function to fetch a random quote from the API
 async function fetchRandomQuote() {
