555b5ca2918a8fd089105f3f7b397640432d93bc
Ada Rachel <adarachelofficial@gmail.com>
2023-10-23 14:41:53 -0700

    get specific character quotes

--- character.js
+++ character.js
@@ -1,29 +1,30 @@
-const characterNameElement = document.getElementById('character-name');
-const characterQuotes = document.getElementById('quote');
+const API_URL = 'https://greyswisdomapi.onrender.com/get';
 
-// Function to parse the query parameter
-function getCharacterName() {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get('name');
-}
-
-// Function to filter and display quotes for the selected character
-function displayCharacterQuotes() {
-    const characterName = getCharacterName();
-    characterNameElement.textContent = `${characterName}'s Quotes`;
-
-    const filteredQuotes = quotes.filter(quote => quote.person === characterName);
-
-    if (filteredQuotes.length === 0) {
-        characterQuotes.textContent = 'No quotes found for this character.';
-    } else {
-        const randomIndex = Math.floor(Math.random() * filteredQuotes.length);
-        characterQuotes.textContent = filteredQuotes[randomIndex].quote;
+// Function to fetch a random quote for a specific character from the API
+async function fetchCharacterQuote(character) {
+    try {
+        const response = await fetch(`${API_URL}?character=${character}`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data.quote;
+    } catch (error) {
+        console.error('Error fetching character quote:', error);
+        return 'An error occurred. Please try again later.';
     }
 }
 
-// Event listener for the "Get Quote" button
-document.getElementById('get-quote').addEventListener('click', displayCharacterQuotes);
+// Function to display the fetched character quote
+async function displayCharacterQuote() {
+    const characterName = getCharacterName();
+    const quoteElement = document.getElementById('quote');
+    const quote = await fetchCharacterQuote(characterName);
+    quoteElement.textContent = quote;
+}
 
-// Display the character's name and a random quote by the selected character
-displayCharacterQuotes();
+// Event listener for the "Get Character's Quote" button
+document.getElementById('get-quote').addEventListener('click', displayCharacterQuote);
+
+// Initial quote display
+displayCharacterQuote();
